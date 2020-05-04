76ad77b6b4f2fccb3280096e71fa93cd0033268f
SGWR2760 <sgwrnatki2760@gmail.com>
2020-05-04 18:40:24 +0900

    remake

---
+++ js/sentence.js
@@ -0,0 +1,37 @@
+const groupName = {
+  jp: "ベトナム交流会",
+  vn: "Đảng trao đổi việt nam",
+  en: "Vietnam Exchange Party"
+}
+
+const informationTitle = {
+  jp: "イベント情報・お役立ち情報",
+  vn: "???",
+  en: "Event information / useful information"
+}
+
+const eventInfo = {
+  jp: "イベント情報",
+  vn: "?????????????????????",
+  en: "Event information"
+}
+const usefulInfo = {
+  jp: "お役立ち情報",
+  vn: "?????????????????????",
+  en: "Useful information"
+}
+const constractionName = {
+  jp: "",
+  vn: "",
+  en: ""
+}
+
+function ins(constName) {
+  document.write(constName);
+}
+
+function test() {
+  var html;
+  html = groupName;
+  document.write(html);
+}
