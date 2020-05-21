03d33c160f26104e1ce4ef3c16775ae4819e5c99
SGWR2760 <sgwrnatki2760@gmail.com>
2020-05-21 22:15:30 +0900

    リンクの追加

--- js/sentence.js
+++ js/sentence.js
@@ -27,13 +27,10 @@ const aboutus = {
   vn: 'vn???????',
   en: '<a class="nav-link" href="' + url + '/en/aboutus/aboutus.html"><i class="fas fa-users"></i> About us</a>'
 }
-
-
-
 const donate = {
   jp: '<a class="nav-link" href="' + url + '/jp/donate/donate.html"><i class="fas fa-hand-holding-usd"></i> 寄付</a>',
   vn: 'vn???????',
-  en: 'Donate'
+  en: '<a class="nav-link" href="' + url + '/en/donate/donate.html"><i class="fas fa-hand-holding-usd"></i> Donate</a>'
 }
 const contactus = {
   jp: '<a class="btn btn-warning headerContactus w-100" href="' + url + '/jp/contactus/contactus.html" role="button"> お問い合わせ</a>',
@@ -49,14 +46,16 @@ const disclaimer = {
 const allEventsInfo = {
   jp: '<a class="nav-link" href="' + url + '/jp/events/"><i class="far fa-calendar-alt"></i> イベント情報一覧</a>',
   vn: 'vn????????',
-  en: 'All event info'
+  en: '<a class="nav-link" href="' + url + '/en/events/"><i class="far fa-calendar-alt"></i> All event info</a>'
 }
+
 
 const allUsefulInfo = {
   jp: '<a class="nav-link" href="' + url + '/jp/information/"><i class="fas fa-info-circle"></i> お役立ち情報一覧</a>',
   vn: 'vn??????',
-  en: 'All topicks'
+  en: '<a class="nav-link" href="' + url + '/en/information/"><i class="fas fa-info-circle"></i> All topicks</a>'
 }
+
 
 const allPastEvents = {
   jp: '<a class="headerLink" href="' + url + '/jp/information/allinfo.html">過去のイベント</a>',
@@ -79,8 +78,10 @@ const pageShowEn = {
 const pageShowVn = {
   jp:'<a class="nav-link" href="'+ url +'/vn/index.html"><i class="fas fa-globe-asia"></i> ベトナム語のページを表示</a>',
   vn:'vn??????',
-  en:'Vietnamese Page'
+  en:'<a class="nav-link" href="'+ url +'/vn/index.html"><i class="fas fa-globe-asia"></i> Vietnamese Page</a>'
 }
+
+
 
 const topPage = {
   jp:'<a class="nav-link headerLink" href="'+ url +'/"><i class="fas fa-home"></i> トップページ</a>',
