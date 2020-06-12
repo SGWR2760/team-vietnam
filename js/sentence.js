9f37a808588013a90d359e73640cf6819e7e7078
SGWR2760 <sgwrnatki2760@gmail.com>
2020-06-13 08:37:17 +0900

    部品の共通化を完了

--- js/sentence.js
+++ js/sentence.js
@@ -23,37 +23,42 @@ const usefulInfo = {
   en: 'Useful information'
 }
 const aboutus = {
-  jp: '<a class="nav-link" href="' + url + '/jp/aboutus/aboutus.html"><i class="fas fa-users"></i> チーム概要</a>',
+  jp: '<a href="' + url + '/jp/aboutus/aboutus.html">チーム概要</a>',
   vn: 'vn???????',
-  en: '<a class="nav-link" href="' + url + '/en/aboutus/aboutus.html"><i class="fas fa-users"></i> About us</a>'
+  en: '<a href="' + url + '/en/aboutus/aboutus.html"> About us</a>'
 }
 const donate = {
-  jp: '<a class="nav-link" href="' + url + '/jp/donate/donate.html"><i class="fas fa-hand-holding-usd"></i> 寄付</a>',
+  jp: '<a href="' + url + '/jp/donate/donate.html"> 寄付</a>',
   vn: 'vn???????',
-  en: '<a class="nav-link" href="' + url + '/en/donate/donate.html"><i class="fas fa-hand-holding-usd"></i> Donate</a>'
+  en: '<a href="' + url + '/en/donate/donate.html"> Donate</a>'
 }
 const contactus = {
-  jp: '<a class="btn btn-warning headerContactus w-100" href="' + url + '/jp/contactus/contactus.html" role="button"> お問い合わせ</a>',
+  jp: '<a href="' + url + '/jp/contactus/contactus.html" role="button"> お問い合わせ</a>',
   vn: 'vn????????',
-  en: '<a class="btn btn-warning headerContactus w-100" href="' + url + '/en/contactus/contactus.html" role="button"> Contact us</a>'
+  en: '<a href="' + url + '/en/contactus/contactus.html" role="button"> Contact us</a>'
 }
 const disclaimer = {
   jp: '<a class="headerLink" href="' + url + '/jp/disclaimer/disclaimer.html">免責事項</a>',
   vn: 'vn????????',
   en: 'disclaimer'
 }
+const privacyPolicy = {
+  jp: '<a class="headerLink" href="' + url + '/jp/disclaimer/privacypolicy.html">プライバシーポリシー</a>',
+  vn: 'vn????????',
+  en: 'disclaimer'
+}
 
 const allEventsInfo = {
-  jp: '<a class="nav-link" href="' + url + '/jp/events/"><i class="far fa-calendar-alt"></i> イベント情報一覧</a>',
+  jp: '<a href="' + url + '/jp/events/"> イベント情報一覧</a>',
   vn: 'vn????????',
-  en: '<a class="nav-link" href="' + url + '/en/events/"><i class="far fa-calendar-alt"></i> All event info</a>'
+  en: '<a href="' + url + '/en/events/"> All event info</a>'
 }
 
 
 const allUsefulInfo = {
-  jp: '<a class="nav-link" href="' + url + '/jp/information/"><i class="fas fa-info-circle"></i> お役立ち情報一覧</a>',
+  jp: '<a href="' + url + '/jp/information/"> お役立ち情報一覧</a>',
   vn: 'vn??????',
-  en: '<a class="nav-link" href="' + url + '/en/information/"><i class="fas fa-info-circle"></i> All topicks</a>'
+  en: '<a href="' + url + '/en/information/"> All topicks</a>'
 }
 
 
@@ -64,29 +69,29 @@ const allPastEvents = {
 }
 
 const pageShowJp = {
-  jp:'<a class="nav-link" href="'+ url +'/index.html"><i class="fas fa-globe-asia"></i> 日本語のページを表示</a>',
+  jp:'<a href="'+ url +'/index.html"> 日本語のページを表示</a>',
   vn:'vn??????',
-  en:'<a class="nav-link" href="'+ url +'/index.html"><i class="fas fa-globe-asia"></i>  Japanese Page</a>'
+  en:'<a href="'+ url +'/index.html"> Japanese Page</a>'
 }
 
 const pageShowEn = {
-  jp:'<a class="nav-link" href="'+ url +'/en/index.html"><i class="fas fa-globe-americas"></i> English Page</a>',
+  jp:'<a href="'+ url +'/en/index.html">English Page</a>',
   vn:'vn??????',
-  en:'<a class="nav-link" href="'+ url +'/en/index.html"><i class="fas fa-globe-americas"></i> English Page</a>'
+  en:'<a href="'+ url +'/en/index.html">English Page</a>'
 }
 
 const pageShowVn = {
-  jp:'<a class="nav-link" href="'+ url +'/vn/index.html"><i class="fas fa-globe-asia"></i> ベトナム語のページを表示</a>',
+  jp:'<a href="'+ url +'/vn/index.html"> ベトナム語のページを表示</a>',
   vn:'vn??????',
-  en:'<a class="nav-link" href="'+ url +'/vn/index.html"><i class="fas fa-globe-asia"></i> Vietnamese Page</a>'
+  en:'<a href="'+ url +'/vn/index.html"> Vietnamese Page</a>'
 }
 
 
 
 const topPage = {
-  jp:'<a class="nav-link headerLink" href="'+ url +'/"><i class="fas fa-home"></i> トップページ</a>',
+  jp:'<a href="'+ url +'/">トップページ</a>',
   vn:'',
-  en:'<a class="nav-link headerLink" href="'+ url +'/en/index.html"><i class="fas fa-home"></i> Home</a>'
+  en:'<a href="'+ url +'/en/index.html">Home</a>'
 }
 
 function ins(constName) {
